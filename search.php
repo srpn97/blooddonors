f3adf0f9de2446316572b04b7dc766603ea5dafb
srpn97 <38552471+srpn97@users.noreply.github.com>
2018-09-05 13:11:50 +0530

    Update search.php

--- search.php
+++ search.php
@@ -13,14 +13,44 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 $search=mysqli_real_escape_string($conn, $_REQUEST['searchp']);
-$criteria=mysqli_real_escape_string($conn, $_REQUEST['criteria']);
 
 echo "Connected successfully";
-$sql = "SELECT SELECT * FROM name WHERE $criteria LIKE $search";
+$sql = "SELECT * FROM blooddonors WHERE bloodtype= ‘$search’ OR email= ‘$search’ OR mobile= ‘$search’";
 $result = $conn->query($sql);
-
-if ($result > 0) {
-	echo $result;
+if($result = mysqli_query($conn, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>name</th>";
+                echo "<th>age</th>";
+                echo "<th>address</th>";
+                echo "<th>email</th>";
+				echo "<th>mobile</th>";
+				echo "<th>group</th>";
+				echo "<th>gender</th>";
+            echo "</tr>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                echo "<td>" . $row['name'] . "</td>";
+                echo "<td>" . $row['age'] . "</td>";
+                echo "<td>" . $row['address'] . "</td>";
+                echo "<td>" . $row['email'] . "</td>";
+				echo "<td>" . $row['mobile'] . "</td>";
+				echo "<td>" . $row['bloodtype'] . "</td>";
+				echo "<td>" . $row['gender'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
 }
+
+
+
 $conn->close();
 ?>
